a60ed0fa55fe01eeabcb88f1c829f2f701b55153
khmizyuk <hmizyk2002@gmail.com>
2022-04-01 16:47:05 +0300

    Adding project

---
+++ senlaHometask4/senlaHometask4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  senlaHometask4
+//
+//  Created by Сергей Хмизюк on 01.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
